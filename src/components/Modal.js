fac540cdd0d7a4550ff77175f0340de17bd56d0c
Dzenis H <dzenis.hankusic@gmail.com>
2020-02-20 19:43:45 +0100

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,13 +5,13 @@ import ReactDOM from "react-dom";
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    // onDismiss could some prop like onDismiss={() => history.push('/')}
+    // onDismiss could have some prop like onDismiss={() => history.push('/')}
     // where the user gets back e.g. to the root path and the Modal gets dismissed
     <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       <div
         // stopPropagation stops the event from bubbling up the event chain
         // We need this to stop Modal from closing when the user clicks inside of it.
-        // --> only if he/ she clicks outside of it (Modal)
+        // --> only if the user clicks outside of it (Modal)
         onClick={e => e.stopPropagation()}
         className="ui standard modal visible active"
       >
